6d5c52274d7b2da816448ed43ab59bc56010eef3
Magnus Teschner <magnus.teschner@mhp.com>
2023-11-09 14:37:31 +0100

    Add game loop and score

--- index.js
+++ index.js
@@ -4,27 +4,58 @@ function getComputerChoice(){
     return options[randomNumber]
 }
 
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase()
-
+function returnResult(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
-        return `draw ${playerSelection} and ${computerSelection} equals out!`
+        let result = [[0,0], `draw ${playerSelection} and ${computerSelection} equals out!`];
+        return result
     } else if (playerSelection === "rock" && computerSelection === "paper"){
-        return "You Lose! Paper beats Rock"
+        let result = [[0,1], 'You Lose! Paper beats Rock'];
+        return result
     } else if (playerSelection === "paper" && computerSelection === "sciccors"){
-        return "You Lose! Sciccors beats Paper"
+        let result = [[0,1], 'You Lose! Sciccors beats Paper'];
+        return result
     } else if (playerSelection === "sciccors" && computerSelection === "rock"){
-        return "You Lose! Rock beats Sciccors"
+        let result = [[0,1], 'You Lose! Rock beats Sciccors'];
+        return result
     } else if (playerSelection === "rock" && computerSelection === "sciccors"){
-        return "You Win! Rock beats Sciccors"
+        let result = [[1,0], 'You Win! Rock beats Sciccors'];
+        return result
     } else if (playerSelection === "paper" && computerSelection === "rock"){
-        return "You Win!! Paper beats Rock"
+        let result = [[1,0], 'You Win! Paper beats Rock'];
+        return result
     } else if (playerSelection === "sciccors" && computerSelection === "paper"){
-        return "You Win!! Sciccors beats Paper"
+        let result = [[1,0], 'You Win! Sciccors beats Paper'];
+        return result
     }
 }
 
-let yourChoice = prompt("Enter Rock, Paper or Sciccor!", "rock");
-let computerChoice = getComputerChoice();
-console.log(playRound(yourChoice, computerChoice));
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase()
+    return returnResult(playerSelection, computerSelection)
+}
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        let yourChoice = prompt("Enter Rock, Paper or Sciccor!", "rock");
+        let computerChoice = getComputerChoice();
+        let result = playRound(yourChoice, computerChoice);
+        if (result[0][0] === 1){
+            ownScore += 1;
+        } else if (result[0][1] === 1){
+            computerScore += 1;
+        }
+        console.log(result[1]);
+    }
+    if (ownScore > computerScore){
+        console.log("you won")
+    } else if (ownScore < computerScore){
+        console.log("computer won")
+    } else {
+        console.log("draw")
+    }
+}
+
+let ownScore = 0;
+let computerScore = 0;
+game();
